261b81003441c6b9aa087443e075ce9b32b0ca74
weiqiang <weiqiang@bonc.com.cn>
2025-09-11 18:12:24 +0800

    chore(docker): 在mongo启动脚本中添加状态日志输出

--- docker/product/mongo_27017.sh
+++ docker/product/mongo_27017.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pwd=`pwd`
+echo '启动mongo'
 docker run -d --name mongo -p 27017:27017 -v $pwd/data/mongo_27017_data:/data/db \
   -v $pwd/config/mongo_27017_config:/etc/mongo \
   -e MONGO_INITDB_ROOT_USERNAME=admin \
